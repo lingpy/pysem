241e1cd6928ca00dadfeca983ea368fee73858f0
Mattis List <mattis.list@uni-passau.de>
2025-06-13 11:56:27 +0200

    release 1.1

--- setup.py
+++ setup.py
@@ -1,48 +1,5 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
-setup(
-    name='pysem',
-    version='0.9.dev0',
-    license='MIT',
-    description='Python library for manipulating semantic data in linguistics',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-    ],
-    author='Johann-Mattis List',
-    author_email='mattis.list@uni-passau.de',
-    url='https://github.com/lingpy/pysem',
-    keywords='data',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    include_package_data=True,
-    zip_safe=False,
-    platforms='any',
-    python_requires='>=3.5',
-    install_requires=[
-        'clldutils>=3.5',
-        'csvw',
-        "attrs>=20"
-    ],
-    extras_require={
-        'dev': ['black', 'wheel', 'twine'],
-        'test': [
-            'pytest>=4.3',
-            'pytest-cov',
-            'coverage>=4.2',
-        ],
-    },
-    entry_points={
-        'console_scripts': [
-        ]
-    },
-)
+setup()
 
--- src/pysem/__init__.py
+++ src/pysem/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.9.dev0"
+__version__ = "1.1"
 from pysem.glosses import to_concepticon
 from pysem.sense import Sense
